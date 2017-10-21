55b88f2936d032f6fdca530ed0f1365ca24dc853
Andy McCall <mailme@andymccall.co.uk>
2017-10-21 23:28:41 +0100

    Added unit test for WebController

--- src/test/java/com/icodossier/controller/WebControllerTest.java
+++ src/test/java/com/icodossier/controller/WebControllerTest.java
@@ -1,7 +1,73 @@
 package com.icodossier.controller;
 
-import static org.junit.Assert.*;
+import com.icodossier.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * The WebControllerTest class is a test class
+ * that tests WebController.
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-10-21
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@TestPropertySource(
+        locations = "classpath:test.properties")
 public class WebControllerTest {
+
+    private MockMvc mockMvc;
+
+     @InjectMocks
+    private WebController webController;
+
+    /**
+     * Sets up objects and mocks external dependencies needed
+     * for the tests.
+     */
+    @Before
+    public void setUp() throws Exception {
+        // this must be called for the @Mock annotations above to be processed
+        // and for the mock service to be injected into the controller under
+        // test.
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(webController).build();
+
+    }
+
+    @Test
+    public void getIndex_IndexIsGot_Passes() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andDo(print());
+    }
+
+    @Test
+    public void getPing_PingIsGot_Passes() throws Exception {
+        this.mockMvc.perform(get("/ping.html"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("ping"))
+                .andDo(print());
+    }
 
 }

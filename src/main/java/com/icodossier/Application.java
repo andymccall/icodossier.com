491145a266572754341033f13cf598f4d9911922
Andy McCall <mailme@andymccall.co.uk>
2017-10-22 17:28:36 +0100

    Added unit test for Application

--- src/main/java/com/icodossier/Application.java
+++ src/main/java/com/icodossier/Application.java
@@ -1,7 +1,5 @@
 package com.icodossier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,14 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * The Application class is the entry point to the main program
  *
  * @author Andy McCall, mailme@andymccall.co.uk
- * @version 0.2
+ * @version 0.3
  * @since 2017-10-21
  */
 @SpringBootApplication
 public class Application {
-
-    private static final Logger logger =
-            LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/test/java/com/icodossier/ApplicationTest.java
+++ src/test/java/com/icodossier/ApplicationTest.java
@@ -6,15 +6,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
+/**
+ * The ApplicationTest class is a test class that
+ * tests the Application class
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-10-22
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @TestPropertySource(
         locations = "classpath:test.properties")
 public class ApplicationTest {
 
+    /**
+     * Tests Application.main()
+     */
     @Test
-    public void Application_applicationContextLoads_Passes() {
+    public void main_NoErrorOutput_Passes() throws Exception {
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(outContent));
+
+        String[] args = {};
+
+        try {
+            Application.main(args);
+        } catch (Exception exception) {
+        }
+
+        assertEquals("Application.main() has failed", "", outContent.toString());
+
     }
 }

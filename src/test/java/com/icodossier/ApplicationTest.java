d27de9d2aef30ae7f84f0362f7eb6425624e5b67
Andy McCall <mailme@andymccall.co.uk>
2017-10-23 17:06:35 +0100

    Update ApplicationTest.java

--- src/test/java/com/icodossier/ApplicationTest.java
+++ src/test/java/com/icodossier/ApplicationTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * The ApplicationTest class is a test class that
